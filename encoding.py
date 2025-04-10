5f719d4c8437c5b81138675f1ba4d5051f22392c
Legacywave1 <105254642+Legacywave1@users.noreply.github.com>
2025-04-10 22:28:17 +0000

    Create encoding.py

---
+++ encoding.py
@@ -0,0 +1,61 @@
+feature_mappings = {
+    'Education': {
+        'No formal education': 0,
+        'Other/Dont know': 1,
+        'Primary education': 2,
+        'Secondary education': 3,
+        'Tertiary education': 4,
+        'Vocational/Specialised training': 5
+    },
+    'most_used_financial_service': {
+        'ATM': 0,
+        'Bank': 1,
+        'None of these': 2
+    },
+    'Area_type': {
+        'Rural': 0,
+        'Urban': 1
+    },
+    'Remittances': {
+        'Not remitting': 0,
+        'Remiting through family and friends': 1,
+        'Remitting informally': 2,
+        'Remitting through bank': 3,
+        'Remitting through other formal (non-bank)': 4
+    },
+    'Saving': {
+        'Not saving': 0,
+        'Saving at bank': 1,
+        'Saving at home': 2,
+        'Saving at other formal (non-bank)': 3,
+        'Saving informally': 4
+    },
+    'Credit': {
+        'Credit from bank': 0,
+        'Credit from family and friends': 1,
+        'Credit from informal sources': 2,
+        'Credit from other formal (non-bank)': 3,
+        'Not borrowing': 4
+    },
+    'Region': {
+        'Ashanti': 0,
+        'Brong Ahafo': 1,
+        'Central': 2,
+        'Eastern': 3,
+        'Greater Accra': 4,
+        'Northern': 5,
+        'Upper East': 6,
+        'Upper West': 7,
+        'Volta': 8,
+        'Western': 9
+    },
+    'Income_Sources': {
+        'Dont Know': 0,
+        'Farming and Fishing': 1,
+        'Formally employed Private': 2,
+        'Informally employed': 3,
+        'Other Income': 4,
+        'Remittance Dependent': 5,
+        'Self employed': 6
+    }
+}
